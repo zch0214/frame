9a67d769bbd03b6588b82a26cc0a6bf0cdd0a66a
zhouchuhang <zch0214@gmail.com>
2019-12-27 15:40:16 +0800

    initialize frame

---
+++ src/main/java/com/zch/frame/FrameApplication.java
@@ -0,0 +1,13 @@
+package com.zch.frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameApplication.class, args);
+	}
+
+}
